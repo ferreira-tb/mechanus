95e59f1259087f566fc32a31b98c2f7207961dec
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2023-03-12 10:58:57 -0300

    fix: exporta tipos que estavam faltando

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export { Mechanus } from '@/mechanus';
-export { ref, unref, isRef } from '@/reactivity/ref';
+export { ref, unref, isRef, isPrimitive } from '@/reactivity/ref';
+export { computed } from '@/reactivity/computed';
 export { watch } from '@/reactivity/effect';
 export { storeToRefs } from '@/store';
-export { computed } from '@/reactivity/computed';
 
-export type { MechanusRef } from '@/reactivity/ref';
+export type { MechanusRef, MechanusRefOptions, UnwrapRef } from '@/reactivity/ref';
+export type { MechanusComputedRef, UnwrapComputed } from '@/reactivity/computed';
+export type { ReactiveCallback, EffectOptions, WatchOptions, ReactiveEffect } from '@/reactivity/effect';
+export type { MechanusStore, StoreRefs, StoreRawValues } from '@/store';
